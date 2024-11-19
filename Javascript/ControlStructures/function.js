68baf9615f1e0c6b381b38906191b6f9e6e6b1de
Batboy-joke <jomilojuolowo@gmail.com>
2024-11-19 04:37:34 -0800

    first commit

---
+++ Javascript/ControlStructures/function.js
@@ -0,0 +1,38 @@
+// function
+// function is a block of code that peforms an action and is
+// reuseable1
+
+// types of function
+// function declaration
+// function expresssion
+
+// let x = myFunction(4, 3);
+
+// function myFunction(a, b) {
+// // Function returns the product of a and b
+//   return a * b;
+// }
+
+// const myFunction = add()
+// 1. function declaration.
+function add(a,b){
+    return(a+b)
+}
+
+const sum = add(2,4);
+console.log(sum)
+
+function mult(a,b){
+    return(a*b)
+}
+
+const product = mult(4,8)
+console.log(product)
+
+function greet(namee){
+    console.log("Hello $(name) Welcome to our app")
+}
+greet("Emmanuel")
+
+const name = "Jomiloju"
+    console.log("hello $(name)")
