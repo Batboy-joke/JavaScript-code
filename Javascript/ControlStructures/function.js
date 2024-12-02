c82f3efca498cf9fb2e2f8b6513b4b50c3a28ced
Batboy-joke <jomilojuolowo@gmail.com>
2024-12-02 04:28:54 -0800

    first commit

--- Javascript/ControlStructures/function.js
+++ Javascript/ControlStructures/function.js
@@ -29,10 +29,39 @@ function mult(a,b){
 const product = mult(4,8)
 console.log(product)
 
-function greet(namee){
+function greet(name){
     console.log("Hello $(name) Welcome to our app")
 }
 greet("Emmanuel")
 
 const name = "Jomiloju"
-    console.log("hello $(name)")
+console.log("hello $(name)")
+
+function infor(name, course){
+    console.log("My name is $(name), I am %(coures) at New Horizion")
+}
+
+infor("john","frontend dev")
+
+// function expresstion
+const add2 = function (a,b){
+    return a+b
+}
+const sum2 = add(1,2)
+console.log(sum2)
+
+const fair = function(hello,Jomiloju){
+    return hello
+}
+const greeting = fair("hello jomiloju")
+console.log(greeting)
+
+const greet3 = function(name){
+    console.log("Hello %(name)")
+}
+greet3("Jomiloju")
+
+function info(name,course,school){
+    console.log("My name is $(name) jomiloju, I am %(course) Frountend dev , I attend %(school) Lafrogramsz ")
+}
+info("jomiloju","Frountend dev","Lafrograms")
